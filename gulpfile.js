3f99838948b6553e57e482859af7549ea5f45a4a
miriam hoffman <miriamhoffman15@gmail.com>
2021-01-13 22:47:45 -0500

    added

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
 
 gulp.task('default', function() {
   // place code for your default task here
+return gulp.src("css/app.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("app.css"));
+
 });
